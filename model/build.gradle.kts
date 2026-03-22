0cb3d57cda640b03d390f64dde28240b2d5247f6
specmatic-builder <github-service-account@specmatic.io>
2026-03-22 06:05:41 +0530

    chore(deps): update dependency com.fasterxml.jackson.module:jackson-module-kotlin to v2.21.2 (#13)

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.21")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.21.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.21.2")
 
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.28.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
