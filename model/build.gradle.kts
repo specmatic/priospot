585ab85f3ac8ac78046dbaaf1dc297f943eb9d50
specmatic-builder <github-service-account@specmatic.io>
2026-07-09 06:13:32 +0530

    chore(deps): update jackson monorepo to v2.22.1 (#29)

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.22")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.22.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.22.1")
 
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.28.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
